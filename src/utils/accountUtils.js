ce97a9d51b35a5bd4b4f3d9bb80effacd1e57d7c
Toyehamzat <anibiseun@gmail.com>
2024-12-11 13:00:30 +0100

    Fix account number generation logic in utility function

--- src/utils/accountUtils.js
+++ src/utils/accountUtils.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto');
 
 exports.generateAccountNumber = () => {
-  return '0' + Math.floor(Math.random() * 9000000000) + 10000000;
-};
+    return '0' + (Math.floor(Math.random() * 9000000000) + 1000000000);
+  };
 
 exports.generateReferenceId = () => {
   return crypto.randomBytes(16).toString('hex');
