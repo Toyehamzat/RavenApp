9351218cf8928603a34ce63c946fbe91b68caae1
Toyehamzat <anibiseun@gmail.com>
2024-12-12 12:34:12 +0100

    Add deposit and transaction routes to main application

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/auth');
-
+const depositRoutes = require('./routes/deposit');
+const transactionRoutes = require('./routes/transaction');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +11,8 @@ app.use(express.json());
 
 // Routes
 app.use('/auth', authRoutes);
-
+app.use('/webhook', depositRoutes);
+app.use('/transaction',transactionRoutes)
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Click to open: http://localhost:${PORT}`);
